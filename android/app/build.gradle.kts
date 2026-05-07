1a1b0351940932208706c02b71810190e66b3e62
alvariascr <luisemilioalvarez22@gmail.com>
2026-05-07 15:32:48 -0600

    Migrate workshop map to Google Maps

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -5,6 +5,16 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val localProperties = java.util.Properties()
+val localPropertiesFile = rootProject.file("local.properties")
+if (localPropertiesFile.exists()) {
+    localPropertiesFile.inputStream().use { localProperties.load(it) }
+}
+
+val googleMapsApiKey = providers.gradleProperty("GOOGLE_MAPS_API_KEY")
+    .orElse(providers.environmentVariable("GOOGLE_MAPS_API_KEY"))
+    .getOrElse(localProperties.getProperty("GOOGLE_MAPS_API_KEY", ""))
+
 android {
     namespace = "com.example.autolab_customer"
     compileSdk = flutter.compileSdkVersion
@@ -28,6 +38,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders["googleMapsApiKey"] = googleMapsApiKey
     }
 
     buildTypes {
--- lib/features/map/presentation/utils/map_utils.dart
+++ lib/features/map/presentation/utils/map_utils.dart
@@ -1,19 +1,17 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../../workshops/domain/entities/workshop.dart';
 
-List<Marker> buildMarkers(List<Workshop> workshops) {
+Set<Marker> buildMarkers(List<Workshop> workshops) {
   return workshops
       .where((workshop) => workshop.hasValidCoordinates)
       .map(
         (w) => Marker(
-          point: LatLng(w.latitude, w.longitude),
-          width: 44,
-          height: 44,
-          child: const Icon(Icons.location_on, color: Colors.red, size: 32),
+          markerId: MarkerId('workshop-${w.id}'),
+          position: LatLng(w.latitude, w.longitude),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          infoWindow: InfoWindow(title: w.name),
         ),
       )
-      .toList();
+      .toSet();
 }
