2f2fe2b2e8944d377c4f38ea7bc84389fb5f0d29
Luis Felipe Murillo <luisfemurillo28@gmail.com>
2026-06-04 16:06:22 -0600

    fix: cerrar flujo de agenda con pop

--- lib/features/workshops/presentation/pages/workshop_appointment_page.dart
+++ lib/features/workshops/presentation/pages/workshop_appointment_page.dart
@@ -296,11 +296,20 @@ class _WorkshopAppointmentPageState extends State<WorkshopAppointmentPage> {
 
   void _handleBack(BuildContext context, AppointmentState state) {
     if (state.currentStep == 0) {
-      _goToWorkshopProfileOrHome(context);
+      _closeAppointmentFlow(context);
       return;
     }
 
     context.read<AppointmentCubit>().goBack();
+  }
+
+  void _closeAppointmentFlow(BuildContext context) {
+    if (context.canPop()) {
+      context.pop();
+      return;
+    }
+
+    _goToWorkshopProfileOrHome(context);
   }
 
   void _goToWorkshopProfileOrHome(BuildContext context) {
