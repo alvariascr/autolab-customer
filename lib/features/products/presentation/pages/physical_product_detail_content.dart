b0479c1343643bf1ca2984ed6cd39c3b2302f187
Luis Felipe Murillo <luisfemurillo28@gmail.com>
2026-06-26 17:02:29 -0600

    refactor: extract shared product detail hero

--- lib/features/products/presentation/pages/physical_product_detail_content.dart
+++ lib/features/products/presentation/pages/physical_product_detail_content.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-import '../../../../core/router/build_context_navigation.dart';
 import '../../../../core/theme/autolab_customer.dart';
 import '../../../../l10n/app_localizations.dart';
 import '../../domain/entities/product.dart';
-import '../widgets/product_image.dart';
 import '../widgets/product_price_text.dart';
+import 'product_detail_hero.dart';
 
 class PhysicalProductDetailContent extends StatefulWidget {
   const PhysicalProductDetailContent({super.key, required this.product});
@@ -38,7 +37,7 @@ class _PhysicalProductDetailContentState
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(
-            child: _ProductHero(
+            child: ProductDetailHero(
               product: product,
               isFavorite: _isFavorite,
               onFavoriteTap: () {
@@ -179,148 +178,6 @@ class _PhysicalProductDetailContentState
       default:
         return l10n.productDetailTypeFallback;
     }
-  }
-}
-
-class _ProductHero extends StatelessWidget {
-  const _ProductHero({
-    required this.product,
-    required this.isFavorite,
-    required this.onFavoriteTap,
-  });
-
-  final Product product;
-  final bool isFavorite;
-  final VoidCallback onFavoriteTap;
-
-  @override
-  Widget build(BuildContext context) {
-    final heroHeight = AutolabCustomer.responsiveDouble(
-      context,
-      compact: 238,
-      regular: 270,
-      tablet: 340,
-    );
-    final avatarSize = AutolabCustomer.responsiveDouble(
-      context,
-      compact: 76,
-      regular: 88,
-      tablet: 104,
-    );
-
-    return SizedBox(
-      height: heroHeight + avatarSize / 2,
-      child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          ProductImage(
-            imageUrl: product.primaryImageUrl,
-            height: heroHeight,
-            placeholderIconSize: AutolabCustomer.iconLg * 1.5,
-          ),
-          Positioned(
-            left: AutolabCustomer.spacingMd,
-            top: MediaQuery.paddingOf(context).top + AutolabCustomer.spacingSm,
-            child: _HeroActionButton(
-              icon: Icons.arrow_back_rounded,
-              onTap: () {
-                context.popOrGo(
-                  product.workshopId.isEmpty
-                      ? '/home-customer'
-                      : '/workshops/${product.workshopId}',
-                );
-              },
-            ),
-          ),
-          Positioned(
-            right: AutolabCustomer.spacingMd,
-            top: MediaQuery.paddingOf(context).top + AutolabCustomer.spacingSm,
-            child: _HeroActionButton(
-              icon: isFavorite
-                  ? Icons.favorite_rounded
-                  : Icons.favorite_border_rounded,
-              foregroundColor: AutolabCustomer.primary,
-              onTap: onFavoriteTap,
-            ),
-          ),
-          Positioned(
-            left: 0,
-            right: 0,
-            top: heroHeight - avatarSize / 2,
-            child: Center(
-              child: _WorkshopAvatar(product: product, size: avatarSize),
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class _HeroActionButton extends StatelessWidget {
-  const _HeroActionButton({
-    required this.icon,
-    required this.onTap,
-    this.foregroundColor = AutolabCustomer.white,
-  });
-
-  final IconData icon;
-  final VoidCallback onTap;
-  final Color foregroundColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return Material(
-      color: AutolabCustomer.secondary.withValues(alpha: 0.62),
-      shape: const CircleBorder(),
-      child: InkWell(
-        customBorder: const CircleBorder(),
-        onTap: onTap,
-        child: SizedBox(
-          width: 40,
-          height: 40,
-          child: Icon(
-            icon,
-            color: foregroundColor,
-            size: AutolabCustomer.iconMd,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _WorkshopAvatar extends StatelessWidget {
-  const _WorkshopAvatar({required this.product, required this.size});
-
-  final Product product;
-  final double size;
-
-  @override
-  Widget build(BuildContext context) {
-    final avatarUrl = product.workshopAvatarUrl.trim();
-
-    return Container(
-      width: size,
-      height: size,
-      padding: const EdgeInsets.all(3),
-      decoration: BoxDecoration(
-        color: AutolabCustomer.customerBackgroundColor(context),
-        shape: BoxShape.circle,
-        border: Border.all(color: AutolabCustomer.customerBorderColor(context)),
-      ),
-      child: CircleAvatar(
-        backgroundColor: AutolabCustomer.customerSurfaceColor(context),
-        backgroundImage: avatarUrl.isNotEmpty ? NetworkImage(avatarUrl) : null,
-        child: avatarUrl.isEmpty
-            ? Icon(
-                Icons.storefront_rounded,
-                color: AutolabCustomer.primary,
-                size: size * 0.4,
-              )
-            : null,
-      ),
-    );
   }
 }
 
