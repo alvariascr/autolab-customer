07e52c7b18aa9305573416c57aa623aafd627b40
Luis Felipe Murillo <luisfemurillo28@gmail.com>
2026-07-27 16:28:36 -0600

    test: cubrir error al cambiar vehículo principal

--- test/features/profile/application/garage_vehicle_controller_test.dart
+++ test/features/profile/application/garage_vehicle_controller_test.dart
@@ -19,4 +19,21 @@ void main() {
     verify(() => setDefaultGarageVehicle('vehicle-1')).called(1);
     expect(notifications, 1);
   });
+
+  test('propagates errors without notifying screens', () async {
+    final setDefaultGarageVehicle = _MockSetDefaultGarageVehicle();
+    final error = Exception('garage_vehicle_not_found_or_not_owned');
+    when(() => setDefaultGarageVehicle(any())).thenThrow(error);
+    final controller = GarageVehicleController(setDefaultGarageVehicle);
+    var notifications = 0;
+    controller.addListener(() => notifications++);
+
+    await expectLater(
+      controller.setDefaultVehicle('vehicle-1'),
+      throwsA(same(error)),
+    );
+
+    verify(() => setDefaultGarageVehicle('vehicle-1')).called(1);
+    expect(notifications, 0);
+  });
 }
