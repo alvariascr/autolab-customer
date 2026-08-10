5ae605401cad410f3029cf7a00d785edb25139ca
alvariascr <luisemilioalvarez22@gmail.com>
2026-08-10 10:30:47 -0600

    Align workshop hero back button style

--- lib/features/workshops/presentation/pages/workshop_profile_hero.dart
+++ lib/features/workshops/presentation/pages/workshop_profile_hero.dart
@@ -46,7 +46,7 @@ class _ProfileHeroState extends State<_ProfileHero> {
             top: topPadding + AutolabCustomer.spacingSmd,
             left: AutolabCustomer.spacingSmd,
             child: _HeroIconButton(
-              icon: Icons.arrow_back_ios_new_rounded,
+              icon: Icons.arrow_back_rounded,
               darkBackground: true,
               onTap: () {
                 if (context.canPop()) {
@@ -214,16 +214,18 @@ class _HeroIconButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final buttonSize = AutolabCustomer.responsiveDouble(
-      context,
-      compact: 38,
-      regular: 44,
-      tablet: 50,
-    );
+    final buttonSize = darkBackground
+        ? 40.0
+        : AutolabCustomer.responsiveDouble(
+            context,
+            compact: 38,
+            regular: 44,
+            tablet: 50,
+          );
 
     return Material(
       color: darkBackground
-          ? AutolabCustomer.secondary.withValues(alpha: 0.68)
+          ? AutolabCustomer.secondary.withValues(alpha: 0.62)
           : AutolabCustomer.white.withValues(alpha: 0.9),
       shape: const CircleBorder(),
       child: InkWell(
@@ -239,12 +241,14 @@ class _HeroIconButton extends StatelessWidget {
                 (darkBackground
                     ? AutolabCustomer.white
                     : AutolabCustomer.secondary),
-            size: AutolabCustomer.responsiveDouble(
-              context,
-              compact: 20,
-              regular: 24,
-              tablet: 28,
-            ),
+            size: darkBackground
+                ? AutolabCustomer.iconMd
+                : AutolabCustomer.responsiveDouble(
+                    context,
+                    compact: 20,
+                    regular: 24,
+                    tablet: 28,
+                  ),
           ),
         ),
       ),
