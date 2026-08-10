e964186a171f907f107f45a53a1a9c65369becc5
Luis Felipe Murillo <luisfemurillo28@gmail.com>
2026-08-10 13:30:38 -0600

    fix: validar checkout desde caso de uso

--- lib/features/cart/domain/usecases/create_cart_order.dart
+++ lib/features/cart/domain/usecases/create_cart_order.dart
@@ -7,6 +7,14 @@ class CreateCartOrder {
   final CartRepository _repository;
 
   Future<CartCheckoutResult> call(CartCheckoutRequest request) {
+    if (request.products.isEmpty) {
+      throw const CartCheckoutException('cart_products_required');
+    }
+
+    if (request.homeDelivery && request.deliveryDetails == null) {
+      throw const CartCheckoutException('cart_delivery_details_required');
+    }
+
     return _repository.createOrder(request);
   }
 }
