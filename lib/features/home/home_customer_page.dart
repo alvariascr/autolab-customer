160f2601baf39e59fd1cfc02047bb36252776c16
Luis Felipe Murillo <luisfemurillo28@gmail.com>
2026-08-19 13:58:06 -0600

    refactor: hacer inmutable el future de talleres del home

--- lib/features/home/home_customer_page.dart
+++ lib/features/home/home_customer_page.dart
@@ -73,7 +73,7 @@ class _HomeCustomerPageState extends State<HomeCustomerPage>
     with WidgetsBindingObserver {
   static const _workshopProximityFilter = WorkshopProximityFilter();
   static const _workshopEmptyStateResolver = WorkshopEmptyStateResolver();
-  late Future<Either<Failure, List<Workshop>>> _workshopsFuture;
+  late final Future<Either<Failure, List<Workshop>>> _workshopsFuture;
   late final HomeServiceFilterCubit _homeServiceFilterCubit;
   final TextEditingController _searchController = TextEditingController();
   final ScrollController _homeScrollController = ScrollController();
