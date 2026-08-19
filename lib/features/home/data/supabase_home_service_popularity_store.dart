eebfcb36555c25815668590ebfdec7ba0d2ef456
Luis Felipe Murillo <luisfemurillo28@gmail.com>
2026-08-19 13:29:37 -0600

    fix: hacer defensivo el parsing del contador de servicios

--- lib/features/home/data/supabase_home_service_popularity_store.dart
+++ lib/features/home/data/supabase_home_service_popularity_store.dart
@@ -13,10 +13,20 @@ class SupabaseHomeServicePopularityStore implements HomeServicePopularityStore {
         .from('home_service_click_counts')
         .select('service_key, click_count');
 
-    return {
-      for (final row in response)
-        row['service_key'] as String: (row['click_count'] as num).toInt(),
-    };
+    final clickCounts = <String, int>{};
+    for (final row in response) {
+      final serviceKey = row['service_key'];
+      if (serviceKey is! String) continue;
+      final normalizedServiceKey = serviceKey.trim();
+      if (normalizedServiceKey.isEmpty) continue;
+
+      final clickCount = row['click_count'];
+      clickCounts[normalizedServiceKey] = clickCount is num
+          ? clickCount.toInt()
+          : 0;
+    }
+
+    return clickCounts;
   }
 
   @override
